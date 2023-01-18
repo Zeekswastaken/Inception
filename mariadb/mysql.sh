ca6268dae8fe7d7706ac4a8bf65f169cb663dc04
yeeticus2000 <ouazahrouz@gmail.com>
2023-01-18 18:27:53 +0100

    Adding mariadb and tying some loose ends

---
+++ mariadb/mysql.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+service mysql start 
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS db;"
+mysql -u root -e "CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'root';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON db.* TO 'user'@'%' ;"
+#mysql -u root -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root@pass'"
+#mysqld
---
+++ wordpress/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sleep 1
+wp core download --allow-root
+
+sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+cp wp-config-sample.php wp-config.php
+
+wp core install --url=example.com --title="Testing" --admin_user=zouazahr --admin_password=Testing2 --admin_email=info@example.com --allow-root
+
+php-fpm7.3 --nodaemonize
