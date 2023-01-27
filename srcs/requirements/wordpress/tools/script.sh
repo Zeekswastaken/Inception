0a12925a21b2e5f21d15b2de6bdf776b74fff89e
yeeticus2000 <ouazahrouz@gmail.com>
2023-01-27 17:22:47 +0100

    Changing the domain name

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,7 +9,7 @@ sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 cp wp-config-sample.php wp-config.php
 chown -R www-data:www-data /var/www/* && mkdir -p /run/php && chmod -R +x /var/www/*
-wp core install --url=localhost --title="Testing" --admin_user=zouazahr --admin_password=Testing2 --admin_email=info@example.com --allow-root
+wp core install --url=zouazahr.42.fr --title="Testing" --admin_user=zouazahr --admin_password=Testing2 --admin_email=info@example.com --allow-root
 wp user create test test@test.com --role=author --allow-root
 
 php-fpm7.3 --nodaemonize
