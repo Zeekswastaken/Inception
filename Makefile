8dfdc6befae2bc45fa1fdf85b147e6b3680ae937
yeeticus2000 <ouazahrouz@gmail.com>
2023-01-30 18:13:18 +0100

    Fixing a sql command

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 re:
 	@docker-compose -f ./srcs/docker-compose.yml build --no-cache
 up:
-	@docker-compose -f ./srcs/docker-compose.yml up --build
+	@docker-compose -f ./srcs/docker-compose.yml up
 down:
 	@docker-compose -f ./srcs/docker-compose.yml down
 
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,7 +8,6 @@ sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 cp wp-config-sample.php wp-config.php
-chown -R www-data:www- /vadatar/www/* && mkdir -p /run/php && chmod -R +x /var/www/*
 wp core install --url=localhost --title="Testing" --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=info@example.com --allow-root
 wp user create test test@test.com --role=author --allow-root
 
