315fded6239547ea7ad01a7f913512a0b88a8bea
yeeticus2000 <ouazahrouz@gmail.com>
2023-02-01 17:16:21 +0100

    fixing the script

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -1,9 +1,9 @@
-#!bin/sh
+#!/bin/sh
 service mysql start 
 echo "CREATE DATABASE IF NOT EXISTS db ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS 'zouazahr'@'%' IDENTIFIED BY 'Testing2' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON db.* TO 'zouazahr'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 mysql < db1.sql
 kill $(cat /var/run/mysqld/mysqld.pid)
